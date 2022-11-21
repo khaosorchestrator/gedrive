eb93b0c8ea48f6a969b91365b9d5a3b1076f504c
Lucas Almeida <developer.lucas24@gmail.com>
2022-11-21 13:35:55 +0100

    chore: add project files

---
+++ src/main/java/com/ldsa/googledriverestapi/GoogledriverestapiApplication.java
@@ -0,0 +1,13 @@
+package com.ldsa.googledriverestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogledriverestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogledriverestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ldsa/googledriverestapi/GoogledriverestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldsa.googledriverestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogledriverestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
