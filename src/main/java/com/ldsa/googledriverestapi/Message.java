5e08957be8609938582e818503b611f3d862d760
Lucas Almeida <developer.lucas24@gmail.com>
2022-11-21 15:56:45 +0100

    initial commit

---
+++ src/main/java/com/ldsa/googledriverestapi/Message.java
@@ -0,0 +1,15 @@
+package com.ldsa.googledriverestapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    private String message;
+}
