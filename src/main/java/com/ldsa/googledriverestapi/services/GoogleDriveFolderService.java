6ec475e75b9b243eccf7afecf39c03338b9379b2
Lucas Almeida <developer.lucas24@gmail.com>
2022-11-22 15:26:49 +0100

    feat: create GoogleDriveFolderService

---
+++ src/main/java/com/ldsa/googledriverestapi/services/GoogleDriveFolderService.java
@@ -0,0 +1,42 @@
+package com.ldsa.googledriverestapi.services;
+
+import com.google.api.services.drive.model.File;
+import com.ldsa.googledriverestapi.dtos.GoogleDriveFolderDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GoogleDriveFolderService {
+
+    private final GoogleDriveManager googleDriveManager;
+
+    public List<GoogleDriveFolderDTO> findAll() {
+        List<File> folders = googleDriveManager.findAllInFolder("root");
+        List<GoogleDriveFolderDTO> folderDTOS = new ArrayList<>();
+
+        if (folders == null) return folderDTOS;
+
+        folders.forEach(file -> {
+            GoogleDriveFolderDTO dto = new GoogleDriveFolderDTO();
+            dto.setId(file.getId());
+            dto.setName(file.getName());
+            dto.setLink("https://drive.google.com/drive/u/3/folders/" + file.getId());
+            folderDTOS.add(dto);
+        });
+
+        return folderDTOS;
+    }
+
+    public void create(String folderName) {
+        String folderId = googleDriveManager.getFolderId(folderName);
+        System.out.println(folderId);
+    }
+
+    public void delete(String id) {
+        googleDriveManager.deleteFileOrFolderById(id);
+    }
+}
